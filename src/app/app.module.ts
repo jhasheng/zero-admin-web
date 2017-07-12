eb2ae01c042418daf29b3b3847a34006496cbdf8
Yuye <yuye2016@gmail.com>
2017-07-12 18:25:50 +0800

    integration material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
+import 'hammerjs';
+
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core';
+
+/* angular material */
+import { MdCardModule, MdCheckboxModule, MdRadioModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +15,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MdCardModule,
+    MdCheckboxModule,
+    MdRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
