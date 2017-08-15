6ec61cd906322a2aa1ca5e1dafdc33fc98682736
Yuye <yuye2016@gmail.com>
2017-08-15 17:34:33 +0800

    upgrade angular cli

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { ZeroAdminWebPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('zero-admin-web App', () => {
-  let page: ZeroAdminWebPage;
+describe('demo App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new ZeroAdminWebPage();
+    page = new AppPage();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
 import { browser, by, element } from 'protractor';
 
-export class ZeroAdminWebPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
